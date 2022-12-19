edc5c48f30e0ced37784bfa760c95b33e32e4cb4
Eric Stéfano <ericssm96@gmail.com>
2022-12-18 22:40:45 -0300

    Estrutura da navbar criada

---
+++ siderbar-modal/src/context.js
@@ -0,0 +1,9 @@
+import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+    return (
+            <AppContext.Provider value="hello">{children}</AppContext.Provider>
+    )
+}
